25079abd0c5908d5e7c1f98fc637c47b3fb8e846
rrozdoce <felipefelipevilhalva@gmail.com>
2025-05-25 13:07:58 -0400

    .

---
+++ contests/algorithms/graph/dfs.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// implementação do algoritmo de busca em profundidade, baseado no algoritmo apresentado por cormen
+
+void dfs(int source, vector<vector<int>> &graph, vector<int> &dist)
+{
+
+}
+
+int main()
+{
+    return 0;
+}
