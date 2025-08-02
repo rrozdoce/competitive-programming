f4a128fbc85fe09460d0337073a45cda201f8d82
rrozdoce <felipefelipevilhalva@gmail.com>
2025-08-02 14:34:40 -0400

    .

---
+++ contests/coders/beecrowd/Teoria dos numeros/bee_2514/2514.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    return 0;
+}
+
