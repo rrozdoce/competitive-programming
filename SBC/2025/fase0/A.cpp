b273533cbff457ded5913b4d089802330d24951b
rrozdoce <felipefelipevilhalva@gmail.com>
2025-09-26 09:13:34 -0400

    .

--- SBC/2025/fase0/A.cpp
+++ SBC/2025/fase0/A.cpp
@@ -1,32 +1,24 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
+
+#define lli long long int
+#define mk_p make_pair
+#define fir first
+#define sec second
 
 int main(int argc, char const *argv[])
 {
   ios_base::sync_with_stdio(false);cin.tie(NULL);
   cin.tie(NULL);
-  int C, G;
-  while(cin >> C >> G)
-  {
-    if(C == 1)
-    {
-      cout << "vivo e morto" << endl;
-    }
 
-    else if(C == 0)
-    {
-      if(G == 0)
-      {
-        cout << "morto" << endl;
-      }
-      else if(G == 1)
-      {
-        cout << "vivo" << endl;
-      }
-    }
-  }
+  string r[2][2];
+  r[0][0] = "morto";
+  r[0][1] = "vivo";
+  r[1][0] = r[1][1] = "vivo e morto";
+  int a,b;
+  cin >> a >> b;
+  cout << r[a][b] << "\n";
 
-  return 0;
 }
 
--- SBC/2025/fase0/B.cpp
+++ SBC/2025/fase0/B.cpp
@@ -1,12 +1,14 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
+
+#define lli long long int
+#define mk_p make_pair
+#define fir first
+#define sec seconds
 
 int main(int argc, char const *argv[])
 {
   ios_base::sync_with_stdio(false);cin.tie(NULL);
   cin.tie(NULL);
-
-  return 0;
 }
-
